bb2742c24d4b8acbaaaea1d1b96630d093d744f7
VERGIL-PC\Altair <narala1610@gmail.com>
2018-04-14 20:35:35 +0700

    New

--- 2_hello2.py
+++ 2_hello2.py
@@ -87,13 +87,82 @@
 # g - p(g)/p'(g)
 # g - guess
 # p(g) = g^2 - y
-epsilon = 0.01
-y = 24.0
-guess = y/2.0
-numGuesses = 0
+# epsilon = 0.01
+# y = 24.0
+# guess = y/2.0
+# numGuesses = 0
 
-while abs(guess*guess - y) >= epsilon:
-    numGuesses += 1
-    guess = guess - (((guess**2) - y)/(2*guess)) # Newton-Raphson
-print("numGuesses = " + str(numGuesses))
-print("Square root of " + str(y) + " is about " + str(guess))
+# while abs(guess*guess - y) >= epsilon:
+#     numGuesses += 1
+#     guess = guess - (((guess**2) - y)/(2*guess)) # Newton-Raphson
+# print("numGuesses = " + str(numGuesses))
+# print("Square root of " + str(y) + " is about " + str(guess))
+
+## Function
+# def test_funtion(i):
+#     """ 
+#     Input: i, is positive int
+#     Return: True if i is even. Otherwise return False
+#     """
+#     print(i)
+#     return i%2==0
+    
+# a = test_funtion(10)
+# print(a)
+
+# # Ex 1:
+# def f(y):
+#     x = 1
+#     x += 1
+#     print(x)        # 2
+
+# x = 5
+# f(x)
+# print(x)            # 5
+
+# # Ex 2: Khong thay x trong g-scope se tim x o parent scope, only look up
+# def g(y):
+#     print(x)            # 5
+#     print(x+1)          # 6
+
+# x = 5
+# g(x)
+# print(x)                # 5
+
+# # Ex 3: Fail khi tinh toan lam thay doi x ma ko co x trong h-scope
+# def h(y):
+#     x = x+1
+# x = 5
+# h(x)
+# print(x)
+
+# # Ex 4:
+# def g(x):
+#     def h():
+#         print("in h(): x =",x)     # 4
+
+#     x = x+1
+#     print("in g(x): x =", x)       # 4
+#     h()
+#     return x
+
+# x = 3
+# z = g(x)
+# print("in global: x =", x)     # 3
+
+### Ex 5: Khi tinh toan voi x nhung ko lam thay doi x
+# # 5.1
+# a = 10
+# def f(x):
+#     return x + a        # 4
+# a = 3
+# f(1)
+
+# # 5.2
+# x = 12
+# def g(x):
+#     x = x + 1               # x trong g-scope = 13
+#     def h(y):
+#         return x + y        # 19
+#     return h(6)
+# g(x)
